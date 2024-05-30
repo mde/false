343d9f9eea93e05626767fab77e9d39f935351d9
rando <158522817+tj-commits@users.noreply.github.com>
2024-05-30 15:34:58 -0500

    make false immutable

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ let False = require('false-value')
 let f = () => False
 
 module.exports = f
+Object.defineProperty(require.cache, require.resolve('false'), {
+   writable: false,
+   configurable: false,
+   enumerable: true,
+   value: module.exports
+})
